2d89c2910b03b87a81cdc1662ad61dc178ee34b1
Murillo <murillo.jogos8@hotmail.com>
2023-08-23 04:52:20 -0400

    wordpress works

--- srcs/requirements/wordpress/conf/wp-myconfig.sh
+++ srcs/requirements/wordpress/conf/wp-myconfig.sh
@@ -1,25 +1,7 @@
-#!bin/sh
-if [ ! -f "/var/www/wp-config.php" ]; then
-cat << EOF > /var/www/wp-config.php
-<?php
-define( 'DB_NAME', '${DB_NAME}' );
-define( 'DB_USER', '${DB_USER}' );
-define( 'DB_PASSWORD', '${DB_PASS}' );
-define( 'DB_HOST', 'mariadb' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
-define('FS_METHOD','direct');
-\$table_prefix = 'wp_';
-define( 'WP_DEBUG', false );
-if ( ! defined( 'ABSPATH' ) ) {
-define( 'ABSPATH', __DIR__ . '/' );}
-define( 'WP_REDIS_HOST', 'redis' );
-define( 'WP_REDIS_PORT', 6379 );
-define( 'WP_REDIS_TIMEOUT', 1 );
-define( 'WP_REDIS_READ_TIMEOUT', 1 );
-define( 'WP_REDIS_DATABASE', 0 );
-require_once ABSPATH . 'wp-settings.php';
-EOF
+#!/bin/sh
+
+if [ ! -d "/var/www/html" ]; then
+  mkdir -p /var/www/html
 fi
 
 if [ ! -f /var/www/html/wp-config.php ]; then
@@ -50,12 +32,14 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	wp core install \
 		--url="${DOMAIN_NAME}" \
 		--title="${WP_TITLE}" \
-		--admin_user="${DB_ROOT}" \
-		--admin_password="${WP_ROOT_PASSWORD}" \
+		--admin_user="${DB_ROOT_USER}" \
+		--admin_password="${WP_ROOT_PASS}" \
 		--admin_email="${WP_ROOT_EMAIL}"
 
 	# Create wordpress user
-	wp user create "${WP_USER}" "${WP_USER_EMAIL}" \
-		--user_pass="${WP_USER_PASSWORD}" \
+	wp user create "${WP_USER}" "${WP_EMAIL}" \
+		--user_pass="${WP_PASS}" \
 		--role=author
 fi
+
+php-fpm81 -F -R
